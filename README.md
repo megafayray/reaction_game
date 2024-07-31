320ead55cb0494d08efd5dcd952a844f4bdc8f0d
megafayray <fay.brotherston@gmail.com>
2024-07-31 17:19:05 -0400

    Complete gameplay

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 7/31/2024
 - Create first game using Ruby, a reaction timer
+
+Credit:
+- YouTube tutorial from "Code with Mario", called "Build your first simple game while learning programming in ruby"
--- reaction_game.rb
+++ reaction_game.rb
@@ -1,12 +1,36 @@
 require 'ruby2d'
 
-set title: "Hello Triangle"
+set title: "Reaction Game"
+set background: "green"
 
-Triangle.new(
-  x1: 320, y1:  50,
-  x2: 540, y2: 430,
-  x3: 100, y3: 430,
-  color: ['red', 'green', 'blue']
-)
+message = Text.new('Click to begin', x: 20, y: 20)
+game_started = false
+square = nil
+start_time = nil
+duration = nil
+
+on :mouse_down do |event|
+  puts event.x, event.y
+  
+  if game_started
+    if square.contains?(event.x, event.y)
+      duration = ((Time.now - start_time)*1000).round
+      message = Text.new("You took: #{duration} milliseconds. Click to begin", x: 20, y: 20)
+      square.remove
+      game_started = false
+    end
+  else
+    message.remove
+
+    square = Square.new(
+    x: rand(get(:width)-25), y: rand(get(:height)-25),
+    size: 25,
+    color: 'purple',
+    )
+
+    start_time = Time.now
+    game_started = true
+  end
+end
 
 show
